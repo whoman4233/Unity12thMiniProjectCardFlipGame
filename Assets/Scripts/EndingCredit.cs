5a801a9b4d9195d9ea723a1fe66dab518b8bb75b
arara0250 <arara4281@gmail.com>
2025-09-23 19:58:36 +0900

    Create Ending Credit UI

---
+++ Assets/Scripts/EndingCredit.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class EndingCredit : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        transform.position += Vector3.up * 0.5f;
+    }
+}
