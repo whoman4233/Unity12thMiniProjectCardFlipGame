0786475436acec1b873009891f92c831816eaac7
Bari_box. <dhkang0126@naver.com>
2025-09-24 14:55:47 +0900

    스테이지 선택 시 가운데 사진 변경

--- Assets/Scripts/ChooseStage.cs
+++ Assets/Scripts/ChooseStage.cs
@@ -7,9 +7,12 @@ public class ChooseStage : MonoBehaviour
 {
     public static ChooseStage Instance; //ChooseStage를 싱글톤을 만들기 위한 변수
 
+    public Image CenterImage;
+
     public GameObject ChickenBtn;
     public GameObject NormalBtn;
     public GameObject HardBtn;
+
     public GameObject StartBtn;
 
     public int Stage = 2;
@@ -29,16 +32,19 @@ public class ChooseStage : MonoBehaviour
         if (clickButton.name == "EasyBtn")
         {
             Debug.Log("Chicken");
+            CenterImage.sprite = Resources.Load<Sprite>("membercard2");
             Stage = 1;
         }
         else if (clickButton.name == "NormalBtn")
         {
             Debug.Log("Normal");
+            CenterImage.sprite = Resources.Load<Sprite>("membercard8");
             Stage = 2;
         }
         else
         {
             Debug.Log("Hard");
+            CenterImage.sprite = Resources.Load<Sprite>("membercard10");
             Stage = 3;
         }
     }
