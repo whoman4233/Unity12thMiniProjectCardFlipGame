5dd20dea0b81592432ecf72302db6bca05c3fb28
Bari_box. <dhkang0126@naver.com>
2025-09-24 10:02:55 +0900

    스테이지 씬 구성 및 선택한 난이도에 따라 MainScene에 변수를 넘기는 코드 작성

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -11,17 +11,31 @@ public class Board : MonoBehaviour
 
     void Start()
     {
-        int[] arr = { 0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7 };
-        arr = arr.OrderBy(x => Random.Range(0f, 7f)).ToArray();
-        for (int i = 0; i < 16; i++)
-        {
-            GameObject go = Instantiate(card, this.transform);
-            float x = (i / 4) * 1.4f - 2.1f;
-            float y = (i % 4) * 1.4f - 3.0f;
-            go.transform.position = new Vector2(x, y);
-            go.GetComponent<Card>().Settings(arr[i]);
-        }
+        int stageNum = ChooseStage.Instance.Stage;
 
-        GameManager.Instance.cardCount = arr.Length;
+        if (ChooseStage.Instance.Stage == 1)
+        {
+            Debug.Log("Easy");
+            int[] arr = { 0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7 };
+            arr = arr.OrderBy(x => Random.Range(0f, 7f)).ToArray();
+            for (int i = 0; i < 16; i++)
+            {
+                GameObject go = Instantiate(card, this.transform);
+                float x = (i / 4) * 1.4f - 2.1f;
+                float y = (i % 4) * 1.4f - 3.0f;
+                go.transform.position = new Vector2(x, y);
+                go.GetComponent<Card>().Settings(arr[i]);
+            }
+
+            GameManager.Instance.cardCount = arr.Length;
+        }
+        else if (ChooseStage.Instance.Stage == 2)
+        {
+            Debug.Log("Normal");
+        }
+        else if (ChooseStage.Instance.Stage == 3)
+        {
+            Debug.Log("HardStage");
+        }
     }
 }
